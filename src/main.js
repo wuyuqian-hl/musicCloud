78ee9aa5f6a404d7164358ece3372f2dfb8a34a7
wuyuqian-hl <1951606881@qq.com>
2021-03-18 14:24:20 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import router from './Router/router'
+import axios from 'axios'
+import Directive from './components/WindowHeight/directive'
+Vue.use(Directive);
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false
-
+Vue.use(Vant);
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
